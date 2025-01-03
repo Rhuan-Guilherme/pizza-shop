8634ccddb23783b19471bc4c4e42ca4206f4bd61
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-01-03 19:53:56 -0300

    🛤️ Configurando Layouts de rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { DashBoard } from './pages/app/dashboard'
 import { SingIn } from './pages/auth/sing-in'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <DashBoard /> },
-  { path: '/sing-in', element: <SingIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <DashBoard /> }],
+  },
+
+  {
+    path: '/sing-in',
+    element: <AuthLayout />,
+    children: [{ path: '/sing-in', element: <SingIn /> }],
+  },
 ])
