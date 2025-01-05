0ccee03297d64cd6367f6c91caf50102def22774
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-01-04 22:54:49 -0300

    🎨👤 Implementação da troca de tema e adição do menu da conta

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
+import { ToogleTheme } from './theme/toogle-theme'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -20,6 +22,11 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ToogleTheme />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
